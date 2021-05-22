765c9cbef221ee05b36a7fc05adcd35d4470a58a
Gabriel Voicu <voicu_gabriel@ymail.com>
2021-05-23 00:32:11 +0300

    parse the greet command

--- src/app.rb
+++ src/app.rb
@@ -1,10 +1,12 @@
 require "functions_framework"
 require "json"
 require 'pg'
+require 'date'
 
 FunctionsFramework.on_startup do |function|
   require_relative "db"
   require_relative "lib/message"
+  require_relative "lib/parser"
 end
 
 FunctionsFramework.http "send_message" do |request|
@@ -13,4 +15,10 @@ FunctionsFramework.http "send_message" do |request|
   msg.empty? ? "Greetings, fellow humans!!!" : msg
   message = Message.find 1
   message.formatted_text
+end
+
+FunctionsFramework.http "quotes" do |request|
+  parser = Parser.new(request: request)
+
+  parser.date.to_s
 end
