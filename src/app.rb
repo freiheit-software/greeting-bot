07c211d5c09f0bac46777e024872478dea3c0528
Hyumiris <isnotyourid@gmx.de>
2021-05-22 21:27:56 +0200

    function now sends db message via slack api

--- src/app.rb
+++ src/app.rb
@@ -1,6 +1,7 @@
 require "functions_framework"
 require "json"
 require 'pg'
+require 'net/http'
 
 FunctionsFramework.on_startup do |function|
   require_relative "db"
@@ -8,9 +9,15 @@ FunctionsFramework.on_startup do |function|
 end
 
 FunctionsFramework.http "send_message" do |request|
-  input = JSON.parse request.body.read rescue {}
-  msg = input["message"].to_s
-  msg.empty? ? "Greetings, fellow humans!!!" : msg
-  message = Message.find 1
-  message.formatted_text
+  msg = Message.find 1
+  data = {
+    "text" => msg.formatted_text,
+    "channel" => "#hackathon-may-2021"
+  }
+  uri = URI('https://slack.com/api/chat.postMessage')
+  res = Net::HTTP.post(uri,
+	data.to_json,
+	{"Content-Type" => "application/json; charset=UTF-8", "Authorization" => "Bearer " + ENV['SLACK_BOT_TOKEN'] })
+  puts res.body
+  ""
 end
