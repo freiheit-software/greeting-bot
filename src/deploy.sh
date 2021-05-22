56518890e42d9d47e0a61edfae749dbb3e558761
Marc André Freiheit <marcandre@freiheit.software>
2021-05-22 20:13:40 +0200

    Add deploy script

---
+++ src/deploy.sh
@@ -0,0 +1,1 @@
+gcloud functions deploy $1 --runtime=ruby27 --trigger-http --entry-point=$1 --region europe-west3 --env-vars-file=env.yaml --service-account=cloud-functions-invoker@freiheit-software-bots.iam.gserviceaccount.com
