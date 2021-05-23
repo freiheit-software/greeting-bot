4b15536fbc6aca91720e487b940c9fabb7f8ada4
Hyumiris <isnotyourid@gmx.de>
2021-05-23 11:33:45 +0200

    update deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,1 +1,4 @@
+base_dir="$PWD"
+cd "`dirname \"$0\"`"
 gcloud functions deploy $1 --runtime=ruby27 --trigger-http --entry-point=$1 --region europe-west3 --env-vars-file=env.yaml --service-account=cloud-functions-invoker@freiheit-software-bots.iam.gserviceaccount.com
+cd "$base_dir"
